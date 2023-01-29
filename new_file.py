8fc9bf1be36bdc7a70c3b04ad51c69fd353f7281
Susana Reyes <srrzaldivar@gmail.com>
2023-01-29 13:41:57 -0500

    edit new_file.py

--- new_file.py
+++ new_file.py
@@ -1,6 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 x = 1
 print("skugdfksh")
 =======
 print("jfh")
 >>>>>>> 344968a (edit new_file)
+=======
+print("jfh")
+print("nalkdbflfkbl")
+>>>>>>> 9288ece (edit new_file)

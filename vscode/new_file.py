968e935e905b248355bf632ecee8273bd2704157
Susana Reyes <srrzaldivar@gmail.com>
2023-01-29 13:44:28 -0500

    edit new_file.py

--- vscode/new_file.py
+++ vscode/new_file.py
@@ -4,8 +4,13 @@ x = 1
 print("skugdfksh")
 =======
 print("jfh")
+<<<<<<< HEAD
 >>>>>>> 344968a (edit new_file)
 =======
 print("jfh")
 print("nalkdbflfkbl")
 >>>>>>> 9288ece (edit new_file)
+=======
+print("nalkdbflfkbl")
+print()
+>>>>>>> 5d49d69 (edit new_file)

9ec0bd3616b717e330ba56bb26ba8a33ef60cdc7
Susana Reyes <srrzaldivar@gmail.com>
2023-01-30 14:44:10 -0500

    code in new_document

---
+++ new_document.py
@@ -0,0 +1,38 @@
+def create_strings_from_characters(frequencies, string1, string2):
+
+    lis_string1 = []
+    lis_string2 = []
+
+    
+
+    for elem_s1 in string1:
+        if elem_s1 in frequencies and frequencies[elem_s1] >= 1:
+            lis_string1.append(elem_s1)
+            frequencies[elem_s1] -= 1
+    
+    
+    lis_string1 = "".join(lis_string1)
+
+    if lis_string1 != string1:
+        for elem in lis_string1:
+            if elem in frequencies:
+                frequencies[elem] += 1
+    
+
+    for elem_s2 in string2:
+        if elem_s2 in frequencies and frequencies[elem_s2] >= 1:
+            lis_string2.append(elem_s2)
+            frequencies[elem_s2] -= 1
+
+    lis_string2 = "".join(lis_string2)
+
+    print(string1, string2, lis_string1, lis_string2)
+
+    if lis_string1 == string1 and lis_string2 == string2:
+        return 2
+
+    if lis_string1 == string1 or lis_string2 == string2:
+        return 1
+    
+    return 0  
+    
